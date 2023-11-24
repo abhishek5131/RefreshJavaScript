f28df449ac2866f3f1ccb6bd74e164d9dbbace42
Abhishek5131 <abhishek5131singh@gmail.com>
2023-11-24 17:55:53 +0530

    variables refresh

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 455
+var accountEmail = "abhishek2323@gmail.com"
+let accountPassword = "asd455"
+
+accountCity = "Haldwani"
+// accountId = 5
+accountEmail = "abc12@google.com"
+accountPassword = "fghd45"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/* prefer not to use var because of 
+ issue in block scope and functional scope
+ */
