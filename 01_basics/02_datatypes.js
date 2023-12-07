254571af40a357d9fcbdb43fa978931ebfb6c35d
Abhishek5131 <abhishek5131singh@gmail.com>
2023-12-07 17:10:49 +0530

    some more conversions

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,9 +1,9 @@
 let score = null
 let valueInNumber = Number(score)
-console.log(typeof(score))
-console.log(typeof(valueInNumber))
-console.log(score);
-console.log(valueInNumber);
+// console.log(typeof(score))
+// console.log(typeof(valueInNumber))
+// console.log(score);
+// console.log(valueInNumber);
 
 /* Conversions
 ------------------------
@@ -11,3 +11,10 @@ console.log(valueInNumber);
 "33abc" => NaN
 true => 1,  false => 0 (Boolean)
 */
+
+let boolean = "nmm"
+let valaueBoolean = Boolean(boolean)
+let numberBoolean = Number(boolean)
+console.log(boolean);
+console.log(numberBoolean);
+console.log(valaueBoolean);
