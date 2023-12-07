071404456f5b200c27469998beec037875a2e3e7
Abhishek5131 <abhishek5131singh@gmail.com>
2023-12-07 16:46:42 +0530

    dataTypes and conversions

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+let score = null
+let valueInNumber = Number(score)
+console.log(typeof(score))
+console.log(typeof(valueInNumber))
+console.log(score);
+console.log(valueInNumber);
+
+/* Conversions
+------------------------
+"33" => 33 (Number)
+"33abc" => NaN
+true => 1,  false => 0 (Boolean)
+*/
